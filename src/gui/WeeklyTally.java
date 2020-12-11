8de2bfd46bfd7a3279f40087e2c7af8c622bc478
Wandiraa <edu.wandira@gmail.com>
2020-12-11 17:39:10 +0700

    fic weeklytally

--- src/gui/WeeklyTally.java
+++ src/gui/WeeklyTally.java
@@ -20,6 +20,7 @@ import java.util.Locale;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -31,71 +32,57 @@ import maker.ImageIconMaker;
 import maker.JLabelMaker;
 
 @SuppressWarnings("serial")
-public class WeeklyTally extends tally {
+public class WeeklyTally extends JFrame {
 	
 	BorderLayout layout;
 	Container container;
 	
 	JPanel days = new JPanel();
+	JPanel teli = new JPanel();
 
 	Dimension ukuranKotak = new Dimension(60,35);
 
-	JLabel day1 = JLabelMaker.setFont("Mon", 18);
-	JLabel day2 = JLabelMaker.setFont("Tue", 18);
-	JLabel day3 = JLabelMaker.setFont("Wed", 18);
-	JLabel day4 = JLabelMaker.setFont("Thu", 18);
-	JLabel day5 = JLabelMaker.setFont("Fri", 18);
-	JLabel day6 = JLabelMaker.setFont("Sat", 18);
-	JLabel day7 = JLabelMaker.setFont("Sun", 18);
+	JLabel day1 = JLabelMaker.setFont("x", 18);
+	JLabel day2 = JLabelMaker.setFont("y", 18);
+	JLabel day3 = JLabelMaker.setFont("z", 18);
+	JLabel day4 = JLabelMaker.setFont("a", 18);
+	JLabel day5 = JLabelMaker.setFont("b", 18);
+	JLabel day6 = JLabelMaker.setFont("c", 18);
+	JLabel day7 = JLabelMaker.setFont("d", 18);
 	JLabel[] labelDays = new JLabel[] {day1,day2,day3,day4,day5,day6,day7};
 	
-	JLabel tallyDay1 = JLabelMaker.setFont("0", 18);
-	JLabel tallyDay2 = JLabelMaker.setFont("0", 18);
-	JLabel tallyDay3 = JLabelMaker.setFont("0", 18);
-	JLabel tallyDay4 = JLabelMaker.setFont("0", 18);
-	JLabel tallyDay5 = JLabelMaker.setFont("0", 18);
-	JLabel tallyDay6 = JLabelMaker.setFont("0", 18);
-	JLabel tallyDay7 = JLabelMaker.setFont("0", 18);
+	JLabel tallyDay1 = JLabelMaker.setFont("aw", 18);
+	JLabel tallyDay2 = JLabelMaker.setFont("aw", 18);
+	JLabel tallyDay3 = JLabelMaker.setFont("aw", 18);
+	JLabel tallyDay4 = JLabelMaker.setFont("aw", 18);
+	JLabel tallyDay5 = JLabelMaker.setFont("aw", 18);
+	JLabel tallyDay6 = JLabelMaker.setFont("aw", 18);
+	JLabel tallyDay7 = JLabelMaker.setFont("aw", 18);
+	JLabel labelTally[] = new JLabel[] {tallyDay1,tallyDay2,tallyDay3,tallyDay4,tallyDay5,tallyDay6,tallyDay7};
 	
 	
 	public WeeklyTally() {
-		// TODO Auto-generated constructor stub
+		
+		// FRAME SETTINGS
 		super("Pomodoro");
 		ImageIcon tomat = ImageIconMaker.ImageIconMaker("tomato");
 		this.setIconImage(tomat.getImage());
-		
-		layout = new BorderLayout();
-		setLayout(layout);
-		
-//		teli.add(namaHari);
-//		teli.add(jumlah);
-
-		masukinLabelKePanel();
-		
-		
-		add(days, BorderLayout.NORTH);
-		add(teli, BorderLayout.SOUTH);
 		setSize(520,115);
 		setResizable(false);
 		setVisible(true);
+		layout = new BorderLayout();
+		setLayout(layout);
 		
+		//PUT ALL LABELS TO PANEL DAYS AND TELI
+		masukinLabelKePanel();
+		//PUT PANEL DAYS AND TELI TO FRAME
+		add(days, BorderLayout.NORTH);
+		add(teli, BorderLayout.SOUTH);
 		
 	}
 
-	private void susunDays() {
-		
-		for(int i=0;i<7;i++) {
-			LocalDate dateToday = LocalDate.now().plusDays(i+1);
-			DayOfWeek today = dateToday.getDayOfWeek();
-			labelDays[i].setText(today.getDisplayName(TextStyle.SHORT, Locale.US));
-		}
-	}
 
 	private void masukinLabelKePanel() {
-		// TODO Auto-generated method stub
-
-		LineBorder b1 = new LineBorder(Color.BLACK);
-		LineBorder b2 = new LineBorder(Color.RED);
 		
 		day1.setPreferredSize(ukuranKotak);
 		day2.setPreferredSize(ukuranKotak);
@@ -113,22 +100,6 @@ public class WeeklyTally extends tally {
 		tallyDay6.setPreferredSize(ukuranKotak);
 		tallyDay7.setPreferredSize(ukuranKotak);
 		
-		day1.setBorder(b1);
-		day2.setBorder(b1);
-		day3.setBorder(b1);
-		day4.setBorder(b1);
-		day5.setBorder(b1);
-		day6.setBorder(b1);
-		day7.setBorder(b1);
-		
-		tallyDay1.setBorder(b2);
-		tallyDay2.setBorder(b2);
-		tallyDay3.setBorder(b2);
-		tallyDay4.setBorder(b2);
-		tallyDay5.setBorder(b2);
-		tallyDay6.setBorder(b2);
-		tallyDay7.setBorder(b2);
-		
 		days.setLayout(new FlowLayout(1,5,0));
 		teli.setLayout(new FlowLayout(1,5,0));
 		
@@ -141,6 +112,7 @@ public class WeeklyTally extends tally {
 		days.add(day6);
 		days.add(day7);
 		
+		susunTally();
 		teli.add(tallyDay1);
 		teli.add(tallyDay2);
 		teli.add(tallyDay3);
@@ -148,6 +120,43 @@ public class WeeklyTally extends tally {
 		teli.add(tallyDay5);
 		teli.add(tallyDay6);
 		teli.add(tallyDay7);
+		
+		// TODO border ini cuma buat ngecek luas area label. SetBorder bisa didelete.
+				LineBorder b1 = new LineBorder(Color.BLACK);
+				LineBorder b2 = new LineBorder(Color.RED);
+				day1.setBorder(b1);
+				day2.setBorder(b1);
+				day3.setBorder(b1);
+				day4.setBorder(b1);
+				day5.setBorder(b1);
+				day6.setBorder(b1);
+				day7.setBorder(b1);
+				tallyDay1.setBorder(b2);
+				tallyDay2.setBorder(b2);
+				tallyDay3.setBorder(b2);
+				tallyDay4.setBorder(b2);
+				tallyDay5.setBorder(b2);
+				tallyDay6.setBorder(b2);
+				tallyDay7.setBorder(b2);	
 	}
+
+	
+	private void susunDays() {
+		for(int i=0;i<7;i++) {
+			LocalDate dateToday = LocalDate.now().plusDays(i+1);
+			DayOfWeek today = dateToday.getDayOfWeek();
+			labelDays[i].setText(today.getDisplayName(TextStyle.SHORT, Locale.US));
+		}
+	}
+	
+	
+	private void susunTally() {
+		// TODO bikin logic untuk baca jumlah entri each day selama seminggu ini
+		for(int i=0;i<7;i++) {
+//			labelTally[i].setText("blabla hasil logic");
+			labelTally[i].setText("0"); //cuma buat contoh bisa overwrite deklarasi di pas new label di atas
+		}
+	}
+
 
 }
